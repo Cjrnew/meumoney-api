0f60edc8b8b304b2317726e3e566505da585d494
Claudio Junior <Cjrnew@gmail.com>
2020-12-29 13:16:08 -0300

    Created method get and post into CategoryResource

--- src/main/java/com/test/meumoneyapi/resource/CategoryResource.java
+++ src/main/java/com/test/meumoneyapi/resource/CategoryResource.java
@@ -1,11 +1,20 @@
 package com.test.meumoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.test.meumoneyapi.model.Category;
 import com.test.meumoneyapi.repository.CategoryRepository;
@@ -13,12 +22,28 @@ import com.test.meumoneyapi.repository.CategoryRepository;
 @RestController
 @RequestMapping("/categories")
 public class CategoryResource {
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	@GetMapping
 	public List<Category> list() {
 		return categoryRepository.findAll();
 	}
+	
+	@GetMapping(value = "/{id}")
+	public Optional<Category> findById(@PathVariable Long id) {
+		return categoryRepository.findById(id);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
+		Category savedCategory = categoryRepository.save(category);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(savedCategory.getId()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(savedCategory);
+	}
+	
 }
