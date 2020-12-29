a95f86a54b9f13beef0936fdc3d3715360953ea8
Claudio Junior <Cjrnew@gmail.com>
2020-12-29 10:56:55 -0300

    Project created with v01flyway, category entity, repository and resource

---
+++ src/test/java/com/test/meumoneyapi/MeumoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.meumoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeumoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
